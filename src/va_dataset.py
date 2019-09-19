5b7e314d5ff8bc098f262d74651592790d6911d5
Liam Hiley <liamhiley@live.co.uk>
2019-09-19 15:23:54 +0100

    Embeddings DS now normalises both embeddings to [0,1] before concat

--- src/va_dataset.py
+++ src/va_dataset.py
@@ -1,6 +1,9 @@
 import os
 from os.path import join
 
+import copy
+import random
+
 import torch
 import torch.utils.data.dataset as dataset
 
@@ -10,6 +13,7 @@ class EmbeddingDataset(dataset.Dataset):
         self.ds_dir = ds_dir
         self.sample_length = sample_length
         self.f_cache = self._init_cache(activities)
+
     def _init_cache(self, activities):
         cache = []
         if activities:
@@ -19,18 +23,28 @@ class EmbeddingDataset(dataset.Dataset):
         for lbl, activity in enumerate(activities):
             samples = sorted([join(activity, vidname) for vidname in os.listdir(activity)])
             for sample in samples:
-                for i in range(len(os.listdir(sample))/2):
-                    embedding_name = f"{sample}_{i*16}"
-                    cache.append(((embedding_name+"_video.pth"),(embedding_name+"_audio.pth")),lbl)
+                for i in range(len(os.listdir(sample))//2):
+                    embedding_name = f"{sample}/embedding_{i * 16}"
+                    if not os.path.exists(embedding_name+"_video.pth"):
+                        # os.remove(embedding_name+"_audio.pth")
+                        continue
+                    elif not os.path.exists(embedding_name+"_audio.pth"):
+                        # os.remove(embedding_name + "_video.pth")
+                        continue
+                    cache.append((((embedding_name+"_video.pth"),(embedding_name+"_audio.pth")),lbl))
         return cache
+
     def __getitem__(self, idx):
         (v, a), lbl = self.f_cache[idx]
         v = torch.load(v)
+        v = (v - v.min())/(v.max() - v.min())
         a = torch.load(a)
+        a = (a - a.min()) / (a.max() - a.min())
         t = torch.zeros(1,v.shape[1] + a.shape[1])
-        t[0,:v.shape[1]].data = v.data
-        t[a.shape[1]:].data = a.data
+        # t = torch.zeros(1,v.shape[1] + v.shape[1])
+        t[0,:v.shape[1]] = v.data
+        t[0,a.shape[1]:] = a.data
         return t
 
-
-
+    def __len__(self):
+        return len(self.f_cache)
